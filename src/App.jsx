28baefa6bb58a472c9db2577c5c080967234e1be
Raveed <raveedusmani@gmail.com>
2024-10-03 20:39:04 +0500

    Data Persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 const App = () => {
   const [todos, setTodos]=useState([]);
   const [todoValue, setTodoValue]=useState("");
+  function PersistData(newlist){
+    localStorage.setItem('todos',  JSON.stringify({todos:
+      newlist
+    }));
+  }
   function AddTodo(newTodo){
     let newTodos=[...todos, newTodo];
     setTodos(newTodos);
+    PersistData(newTodos);
   }
   function DeleteTodo(index){
     let newTodos=todos.filter( (todo, todoIndex)=>{
       return todoIndex!=index;
     });
     setTodos(newTodos);
+    PersistData(newTodos);
   }
   function EditTodo(index){
     setTodoValue(todos[index]);
     DeleteTodo(index);
   }
+  useEffect(()=>{
+    if(!localStorage){
+      return;
+    }
+    let localTodos=localStorage.getItem('todos');
+    if(!localTodos){
+      return;
+    }
+    setTodos(JSON.parse(localTodos).todos);
+  }, []);
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} AddTodo={AddTodo}/>
